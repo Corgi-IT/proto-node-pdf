2c06444d84e59c74f70ab1b0c69f63337bdde97b
Renzo Roso <renzo@geex.company>
2017-03-10 00:17:50 +0100

    Added some comments to the code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Requires
 const {wrap} = require('co');
 const {join} = require('path');
 const moment = require('moment');
@@ -6,15 +7,13 @@ const thunkify = require('thunkify');
 const read = thunkify(require('fs').readFile);
 const handlebars = require('handlebars');
 
+
+// PDF Options
 const pdf_options = {format: 'A4', quality: 300};
 
-
-
-
-
-
-
+// GeneratePDF
 const generatePDF = wrap(function * () {
+	// Data we're going to pass to Handlebars
 	const data = {
 		mycompany: {
 			name: 'GeeX',
@@ -28,20 +27,25 @@ const generatePDF = wrap(function * () {
 		date_due: moment().add(14, 'days').format('DD/MM/YYYY')
 	};
 
+	// Add customer data
 	data.customer = {
 		org: 'Your Org',
 		name: 'Foo Bar',
 		email: 'mail@domain.com'
 	};
 
+	// Read source template
 	const source = yield read(join(`${__dirname}/template.html`), 'utf-8');
 
+	// Convert to Handlebars template and add the data
 	const template = handlebars.compile(source);
 	const html = template(data);
 
+	// Generate PDF and thunkify the toFile function
 	const p = pdf.create(html, pdf_options);
 	p.toFile = thunkify(p.toFile);
 
+	// Saves the file to the File System as invoice.pdf in the current directory
 	yield p.toFile(`${join(__dirname, 'invoice.pdf')}`);
 
 });
